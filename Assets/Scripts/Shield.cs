326c3a5622a4004fc4a55bf436725402f5d4fb36
ArtinAzd <artinazod85@gmail.com>
2025-04-25 22:32:44 +0330

    shield health show added

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class Shield : MonoBehaviour
 {
     private int health = 5;
+    private TextMeshPro healthShow;
+
+    private void Awake()
+    {
+        healthShow = GetComponentInChildren<TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        healthShow.text = "" + health;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("playerLaser") || other.CompareTag("invaderLaser"))
